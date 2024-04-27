efaadad0840d2cc23211a80b08ccbae8262ea474
Ali Day <ali@k20media.com>
2024-04-27 21:40:18 +0200

    added new kink_for_me theme

---
+++ web/themes/custom/kink_for_me/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.kink_for_me = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
